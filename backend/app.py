2fba4b00621d5779b524282aca9ff4cb97940fe2
inderr50721 <isingh50721@gmail.com>
2026-04-29 00:38:04 +0530

    fixed render port issue

--- backend/app.py
+++ backend/app.py
@@ -13,5 +13,8 @@ def stats():
         "memory": "60%"
     })
 
+import os
+
 if __name__ == "__main__":
-    app.run(port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
